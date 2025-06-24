6943261c205218c103bd149daf4c8a62a064a49a
Agam Singh <agamsgujral@gmail.com>
2025-06-24 13:23:19 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("This is my revision node app for the AWS Devops course by Agam singh ");
+    res.send("This is my revision node app for the AWS Devops course by Agam singh  and i am learning to integrate this app with Jenkins and Docker");
 });
 
 app.listen(8000, () => {
