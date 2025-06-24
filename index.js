6320303f2688e879b7ccf16d9fa7b25dc0e0e86c
Agam Singh <agamsgujral@gmail.com>
2025-06-25 01:46:07 +0530

    added few more lines

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("This is my revision node app for the AWS Devops course by Agam singh  and i am learning to integrate this app with Jenkins and Docker and yes i have done it scuccessgully integrated the jenkins with the github and docker with the jenkins and now i am going to learn AWS ");
+    res.send("This is my revision node app for the AWS Devops course by Agam singh  and i am learning to integrate this app with Jenkins and Docker and yes i have done it scuccessgully integrated the jenkins with the github and docker with the jenkins and now i am going to learn AWS and deploy this app on AWS");
 });
 
 app.listen(8000, () => {
