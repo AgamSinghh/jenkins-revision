f4005cc4c125bd9b6b9f07a684b6540a54186a55
AGAM SINGH <166758714+AgamSinghh@users.noreply.github.com>
2025-06-29 01:04:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Ab ek bari github hook se bhi try krke dekhlete haiii");
+    res.send("Ab ek bari github hook se bhi try krke dekhlete haii");
 });
 
 app.listen(8000, () => {
