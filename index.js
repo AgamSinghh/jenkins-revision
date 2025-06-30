45911c70418b5c40a00d1a79b5d890cb2a88d1d0
AGAM SINGH <166758714+AgamSinghh@users.noreply.github.com>
2025-07-01 00:23:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Done scenee haii");
+    res.send("Github Webhhok bhi hogya");
 });
 
 app.listen(8000, () => {
