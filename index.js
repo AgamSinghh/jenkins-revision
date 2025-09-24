a96bd66923f156ec931eec83fcb0f43c2e567e95
unknown <agamsgujral@gmail.com>
2025-09-24 15:01:46 +0530

    changed desc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Github Webhhok bhi hogya");
+    res.send("HAVE COME A LONG WAY BRO");
 });
 
 app.listen(8000, () => {
